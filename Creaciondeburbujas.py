004551102dfaf8e6aea1d09f134ee2274a5b361f
drbermeo <drbermeo@utpl.edu.ec>
2016-07-24 02:25:49 -0500

    Cambiodeactor
    Se cambio el actor de nave por actor naveroja y se le disminuyo el
    tamaño

--- Creaciondeburbujas.py
+++ Creaciondeburbujas.py
@@ -18,9 +18,9 @@ def comenzar_juego():
         def iniciar(self):
             self.imagen = "azul.png"
             self.aprender( pilas.habilidades.PuedeExplotarConHumo )
-            self.x = pilas.azar(-200, 200)
-            self.y = 290
-            self.velocidad = pilas.azar(10, 40) / 10.0
+            self.x = pilas.azar(-200,200)
+            self.y = 30
+            self.velocidad = pilas.azar(30, 40) / 10.0
             self.escala = 0.5
 
         def actualizar(self):
@@ -36,7 +36,7 @@ def comenzar_juego():
         def iniciar(self):
             self.imagen = "negra.png"
             self.aprender( pilas.habilidades.PuedeExplotar)
-            self.x = pilas.azar(-400, 400)
+            self.x = pilas.azar(-200, 200)
             self.y = 250
             self.velocidad = pilas.azar(10, 20) / 10.0
             self.escala = 0.5
@@ -65,19 +65,38 @@ def comenzar_juego():
             # Elimina el objeto cuando sale de la pantalla.
             if self.y < -500:
                 self.eliminar()
+    class Burbujablanca(pilasengine.actores.Actor):
+        
+        def iniciar(self):
+            self.imagen = "blanca.png"
+            self.aprender( pilas.habilidades.PuedeExplotar)
+            self.x = pilas.azar(-200, 200)
+            self.y = 250
+            self.velocidad = pilas.azar(10, 30) / 10.0
+            self.escala = 0.5
+        def actualizar(self):
+            self.rotacion += 8
+            self.y -= self.velocidad
+
+            # Elimina el objeto cuando sale de la pantalla.
+            if self.y < -500:
+                self.eliminar()
 
     class disparos():
         
         def disparo_doble():
-            nave.aprender(pilas.habilidades.Disparar,
+            naveroja.aprender(pilas.habilidades.Disparar,
             municion=pilas.municion.BalaDoble,
             offset_disparo=(0, 30))
                 
     fondo = pilas.fondos.Galaxia(dy=-5)
     
     enemigos = pilas.actores.Grupo()
-    
-
+    blancas=pilas.actores.Grupo()
+    def Activar_Principal():
+        actor=Burbujablanca(pilas)
+        blancas.agregar(actor)
+        
     def Activar_Enemigo():
         actor =Burbujaazul(pilas)
         enemigos.agregar(actor)
@@ -88,17 +107,23 @@ def comenzar_juego():
         
             
     pilas.tareas.siempre(0.50, Activar_Enemigo)
-    nave = pilas.actores.Nave(y = -200)
-    nave.aprender(pilas.habilidades.LimitadoABordesDePantalla)
-    nave.definir_enemigos(enemigos, puntaje.aumentar)
-    pilas.colisiones.agregar(nave, enemigos, nave.eliminar)
-
-    pilas.avisar("Dispara co ESPACIO y muevete con las FLECHAS del teclado...")
+    pilas.tareas.siempre(0.50, Activar_Principal)
+    naveroja = pilas.actores.NaveRoja(y = -200)
+    naveroja.escala = 0.5
+    naveroja.aprender(pilas.habilidades.LimitadoABordesDePantalla)
+    naveroja.definir_enemigos(blancas,puntaje.aumentar)
+    pilas.colisiones.agregar(naveroja, enemigos, naveroja.eliminar)
+    
+    
+    pilas.avisar("Dispara con ESPACIO y muevete con las FLECHAS del teclado...")
 
     def salir():
         pilas.terminar()
     
     opciones = pilas.interfaz.ListaSeleccion(['Score'], salir)
+    opciones.x = +195
+    opciones.y = +178
+    opciones = pilas.interfaz.ListaSeleccion(['Score2'], salir)
     opciones.x = +195
     opciones.y = +178
 
